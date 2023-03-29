c1f04a4ce6b420ecd00236d57ff9b72f3b8afc0c
NicolaiLee <nicolai.rvyn.lee@codeandtheory.com>
2023-03-29 21:44:51 +0800

    applied change requests; code spacing and EOF cleanup

--- javaScriptNumbers/script2.js
+++ javaScriptNumbers/script2.js
@@ -2,6 +2,6 @@ const wholesalePrice = 5.45;
 const retailPrice = 9.99;
 const quantity = 47;
 
-let salesTotal = retailPrice*quantity;
-let profit = salesTotal - (wholesalePrice*quantity);
+let salesTotal = retailPrice * quantity;
+let profit = salesTotal - ( wholesalePrice * quantity );
 let profitPerUnit = profit / quantity;
