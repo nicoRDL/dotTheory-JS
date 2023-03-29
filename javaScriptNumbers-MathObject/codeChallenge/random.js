ae0eca10e63c845622502a9b26fd37322ded7d47
NicolaiLee <nicolai.rvyn.lee@codeandtheory.com>
2023-03-30 00:55:18 +0800

    added additional code requirement and practice

--- javaScriptNumbers-MathObject/codeChallenge/random.js
+++ javaScriptNumbers-MathObject/codeChallenge/random.js
@@ -1,7 +1,13 @@
-const getInput = prompt('Please input a Number:');
+let getInput;
+let inputInt;
 
-const inputInt = parseInt(getInput);
+getInput = prompt('Please input a Number:');
+
+while (isNaN(getInput)) {
+  getInput = prompt('Invalid Input. Please input a Number:');
+}
+
+inputInt = parseInt(getInput);
 
 const randomNumber = Math.floor(Math.random() * inputInt) + 1;
-
 console.log(`${randomNumber} is a random number between 1 and ${inputInt}` );
