1c9372419d8939ac3014ac691ebd50f0f136620b
NicolaiLee <nicolai.rvyn.lee@codeandtheory.com>
2023-06-21 11:50:23 +0800

    finished challenge problems for Handling the DOM tutorials

---
+++ javaScriptDOM-handling/querySelector/sampleQuerySelector.js
@@ -0,0 +1,3 @@
+const navigationLinks = document.querySelectorAll('nav a');
+const galleryLinks = document.querySelectorAll('#gallery a');
+const footerImages = document.querySelectorAll('footer img');
