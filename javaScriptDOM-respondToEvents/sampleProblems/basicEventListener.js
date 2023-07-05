06b527f343f5867fc45f92a4674a868e22e23ffd
NicolaiLee <nicolai.rvyn.lee@codeandtheory.com>
2023-07-05 14:36:09 +0800

    improved variable declartion

--- javaScriptDOM-respondToEvents/sampleProblems/basicEventListener.js
+++ javaScriptDOM-respondToEvents/sampleProblems/basicEventListener.js
@@ -1,5 +1,5 @@
-var warning = document.getElementById("warning");
-var button = document.getElementById('makeItRed');
+const warning = document.getElementById("warning");
+const button = document.getElementById('makeItRed');
 
 button.addEventListener('click', () => {
   document.getElementById('warning').style.backgroundColor = 'red';
