b7ce51eddcff81d61fe981c7d7ea4ea57042e92b
NicolaiLee <nicolai.rvyn.lee@codeandtheory.com>
2023-07-06 16:51:56 +0800

    finished programming AJAX tutorial

---
+++ javaScriptAJAX-programming/checkResponseStatus/sampleAJAX.js
@@ -0,0 +1,11 @@
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function() {
+  if(xhr.readyState === 4) {
+    if(xhr.status === 200) {
+      const s_bar = document.getElementById('sidebar');
+      s_bar.innerHTML = xhr.responseText;
+    };
+  };
+};
+xhr.open('GET', 'sidebar.html');
+xhr.send();
