293feef36959c25583693e71cc772021fc823ef8
NicolaiLee <nicolai.rvyn.lee@codeandtheory.com>
2023-09-04 01:36:10 +0800

    updated variable name

--- javaScriptAJAX-programming/checkResponseStatus/sampleAJAX.js
+++ javaScriptAJAX-programming/checkResponseStatus/sampleAJAX.js
@@ -2,8 +2,8 @@ const xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function() {
   if(xhr.readyState === 4) {
     if(xhr.status === 200) {
-      const s_bar = document.getElementById('sidebar');
-      s_bar.innerHTML = xhr.responseText;
+      const sidebar = document.getElementById('sidebar');
+      sidebar.innerHTML = xhr.responseText;
     };
   };
 };
