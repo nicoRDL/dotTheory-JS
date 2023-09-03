4d6c1e6f87851ef55c8c6f2e3a6b8d50777bac7c
NicolaiLee <nicolai.rvyn.lee@codeandtheory.com>
2023-09-04 01:42:21 +0800

    refactored code for improved logic

--- javaScriptAJAX-programming/codeChallenge/roomWidget.js
+++ javaScriptAJAX-programming/codeChallenge/roomWidget.js
@@ -1,26 +1,23 @@
-let getRooms =  new XMLHttpRequest();
+const getRooms = new XMLHttpRequest();
 
 getRooms.onreadystatechange = function () {
   if (getRooms.readyState === 4) {
-    if(getRooms.status === 200) {
+    if (getRooms.status === 200) {
       const rooms = JSON.parse(getRooms.responseText);
-      let getRoomStatusHTML = '<ul class = "rooms">';
+      let getRoomStatusHTML = '<ul class="rooms">';
 
-      for(let i = 0; i < rooms.length; i += 1) {
-        if(rooms[i].available) {
-          getRoomStatusHTML += '<li class = "empty">';
+      for (let i = 0; i < rooms.length; i += 1) {
+        if (rooms[i].available) {
+          getRoomStatusHTML += '<li class="empty">';
         } else {
-          getRoomStatusHTML += '<li class = "full">';
+          getRoomStatusHTML += '<li class="full">';
         }
-        getRoomStatusHTML += rooms[i].room;
-        getRoomStatusHTML += '</li>';
+        getRoomStatusHTML += `${rooms[i].room}</li>`;
       }
-      getRoomStatusHTML += '</ul>';
-
-      document.getElementById('roomList').innerHTML = getRoomStatusHTML;
+      getRoomStatusHTML += "</ul>";
+      document.getElementById("roomList").innerHTML = getRoomStatusHTML;
     }
   }
 };
-getRooms.open('GET', 'data/rooms.json');
+getRooms.open("GET", "data/rooms.json");
 getRooms.send();
-
