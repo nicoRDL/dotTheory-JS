95aee5fb315ca1332208272c923a26dd3ca9104f
Nicolai Rvyn Lee <nicolai.rvyn.lee@codeandtheory.com>
2023-09-04 19:50:31 +0800

    JS-301 (JS-354)  AJAX Basics: Programming AJAX (#23)
    * finished challenge problems for Handling the DOM tutorials
    
    * reset main branch
    
    * finished programming AJAX tutorial
    
    * removed console log
    
    * updated variable to const
    
    * added end of line
    
    * updated variable name
    
    * refactored code for improved logic

---
+++ javaScriptAJAX-programming/codeChallenge/roomWidget.js
@@ -0,0 +1,23 @@
+const getRooms = new XMLHttpRequest();
+
+getRooms.onreadystatechange = function () {
+  if (getRooms.readyState === 4) {
+    if (getRooms.status === 200) {
+      const rooms = JSON.parse(getRooms.responseText);
+      let getRoomStatusHTML = '<ul class="rooms">';
+
+      for (let i = 0; i < rooms.length; i += 1) {
+        if (rooms[i].available) {
+          getRoomStatusHTML += '<li class="empty">';
+        } else {
+          getRoomStatusHTML += '<li class="full">';
+        }
+        getRoomStatusHTML += `${rooms[i].room}</li>`;
+      }
+      getRoomStatusHTML += "</ul>";
+      document.getElementById("roomList").innerHTML = getRoomStatusHTML;
+    }
+  }
+};
+getRooms.open("GET", "data/rooms.json");
+getRooms.send();
